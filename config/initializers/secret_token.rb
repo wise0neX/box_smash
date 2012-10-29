95a256c2d6aa587431782a4750ee6b5ab7ebff6a
wise0neX <sisenda.fred@gmail.com>
2012-10-28 22:45:58 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BoxSmash::Application.config.secret_token = '6c4c3104cbde4a612f79b54812808290b6be7d9ed1c38d4a24b0c8e8419cef25225cbc3e635e18d93f341757002dc16079e63bd56e3af2c3503309df2441540f'
