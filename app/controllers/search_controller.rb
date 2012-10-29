5ccf6710d3028b1e0899f7c5f78fc203cfabb223
Ani Saraf <anirudhsaraf@gmail.com>
2012-10-29 15:23:01 -0400

    test

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,18 @@
+require '/opt/local/lib/ruby1.9/gems/1.9.1/gems/dancroak-twitter-search-0.5.8/lib/twitter_search'
 class SearchController < ApplicationController
-
+  
   @value = "no"
   def test
+    
+    
     print "here"
   end
   
   def do_test
     @value =  params[:account]
+    client = TwitterSearch::Client.new('thunderthimble')
+    @tweets = client.query :q => @value, :page => "1"
+    print @tweets
     print @value
   end
 end
